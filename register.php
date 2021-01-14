a9bc50bdfbc18919a3cf802eb4ea0de983e96deb
josh.dieckman@gmail.com <josh.dieckman@gmail.com>
2021-01-14 09:12:55 -0500

    Added error messages to the register page

--- register.php
+++ register.php
@@ -12,6 +12,25 @@
             <input type="password" name="confirmpassword" placeholder="Confirm Password...">
             <button type="submit" name="submit">Sign Up</button>
         </form>
+        <?php
+            if (isset($_GET["error"])){
+                if ($_GET["error"] == "emptyInputRegister"){
+                    echo "<p>Please fill in all fields!</p>";
+                }else if ($_GET["error"] == "invalidUsername"){
+                    echo "<p>This username is invalid!</p>";
+                }else if ($_GET["error"] == "invalidEmail"){
+                    echo "<p>This email is invalid!</p>";
+                }else if ($_GET["error"] == "passwordMatch"){
+                    echo "<p>The passwords did not match!</p>";
+                }else if ($_GET["error"] == "usernameExists"){
+                    echo "<p>This username already exists. Please choose a new username.</p>";
+                }else if ($_GET["error"] == "stmtfailed"){
+                    echo "<p>Something went wrong! Please try again.</p>";
+                }else if ($_GET["error"] == "none"){
+                    echo "<p>Welcome to Gripe!</p>";
+                }
+            }
+        ?>
     </section>
 
 <?php
