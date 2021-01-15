5f4d2380b88599b8598b0519177a361237b0d256
josh.dieckman@gmail.com <josh.dieckman@gmail.com>
2021-01-15 10:55:55 -0500

    reset request page

---
+++ reset-request.php
@@ -0,0 +1,14 @@
+<?php
+
+    if(isset($_POST["reset-request-submit"])){
+        $selector = bin2hex(random_bytes(8));
+        $token = random_bytes(32);
+
+        $url = "www.dieckmandesigns.com/codelou/create-new-password.php?selector=" . $selector . "&validator=" . bin2hex($token);
+
+        $expires = date("U") + 1800;
+    }else{
+        header("location: index.php");
+    }
+
+?>
