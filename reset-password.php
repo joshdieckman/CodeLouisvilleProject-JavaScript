c9166efe74716c9c015903fd8a568c723189848f
josh.dieckman@gmail.com <josh.dieckman@gmail.com>
2021-01-15 10:43:25 -0500

    reset-password page

---
+++ reset-password.php
@@ -0,0 +1,30 @@
+<?php
+//login.php
+
+// Include config file
+require_once "config.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h1>Reset Your Password</h1>
+        <p>An email will be sent to you with instructions on how to reset your password.</p>
+        <form action="reset-request.php" method="post">
+            <input type="text" name="email" placeholder="Enter your email address">
+            <button type="submit" name="reset-request-submit">Receive new password by email</button>
+        </form>
+    </div>
+</body>
+</html>
