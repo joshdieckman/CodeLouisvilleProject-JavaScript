0fab34830d26d276b93d667d5defaa20d21f74f9
josh.dieckman@gmail.com <josh.dieckman@gmail.com>
2021-01-15 12:21:02 -0500

    create-new-password.php

--- create-new-password.php
+++ create-new-password.php
@@ -19,16 +19,25 @@ require_once "config.php";
 </head>
 <body>
     <div class="wrapper">
-        <h1>Reset Your Password</h1>
-        <p>An email will be sent to you with instructions on how to reset your password.</p>
-        <form action="reset-request.php" method="post">
-            <input type="text" name="email" placeholder="Enter your email address">
-            <button type="submit" name="reset-request-submit">Receive new password by email</button>
-        </form>
         <?php
-            if(isset($_GET["reset"])){
-                if($_GET["reset"] == "success"){
-                    echo '<p class="signupsuccess">Check your email!</p>';
+            $selector = $_GET["selector"];
+            $validator = $_GET["validator"];
+
+            if(empty($selector) || empty($validator)){
+                echo "Could not validate your request!";
+            }else{
+                if(ctype_xdigit($selector) !== false && ctype_xdigit($validator) !== false){
+                    ?>
+
+                    <form action="reset-password.php" method="post">
+                        <input type="hidden" name="selector" value="<?php echo $selector; ?>">
+                        <input type="hidden" name="validator" value="<?php echo $validator; ?>">
+                        <input type="password" name="pwd" placeholder="Enter a new password">
+                        <input type="password" name="pwd-repeat" placeholder="Repeat new password">
+                        <button type="submit" name="reset-password-submit">Reset Password</button>
+                    </form>
+
+                    <?php
                 }
             }
         ?>
